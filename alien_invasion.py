f44d81e5ad2a3772d2fbe50eb79f298bf6bb0d85
Asad Mehmood <bcpe223053@cust.pk>
2024-05-12 17:09:59 +0500

    Alien Invasion

---
+++ alien_invasion.py
@@ -0,0 +1,27 @@
+import sys
+import pygame
+from settings import Settings
+# Alien Invasion Game by Asad Mehmood
+class AlienInvasion:
+    
+    def __init__(self):
+        pygame.init()                                                   # Initialize the game and create a game window.
+        self.clock = pygame.time.Clock()                                # Set the clock class to control the frame rate.
+        self.settings = Settings()                                      # Create an instance of Settings class.
+        self.screen=pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))  # Set the screen size.
+        pygame.display.set_caption(self.settings.caption)               # Set the caption of the game window.
+        
+    
+    def run_game(self):
+        while True:                                                     # Start the main loop for the game.
+            for event in pygame.event.get():                            # Watch for keyboard and mouse events.
+                if event.type == pygame.QUIT:                           
+                    sys.exit()                                          #quit the game
+            self.screen.fill(self.settings.bg_color)                    # Redraw the screen during each pass through the loop.
+            pygame.display.flip()                                       # Make the most recently drawn screen visible.
+            self.clock.tick(60)                                         # Set the frame rate to 60 frames per second.
+
+if __name__=='__main__':                                                # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+
+
+class Settings:
+    def __init__(self):
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (120, 81, 169)
+        self.caption = "Alien Invasion 🎮"
