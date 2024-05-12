9ca15cdb57b8c43c77844f7a484d774efda4442a
Asad Mehmood <bcpe223053@cust.pk>
2024-05-12 17:22:01 +0500

    To render a space ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 # Alien Invasion Game by Asad Mehmood
 class AlienInvasion:
     
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.screen=pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))  # Set the screen size.
         pygame.display.set_caption(self.settings.caption)               # Set the caption of the game window.
+        self.ship = Ship(self)                                          # Create an instance of the Ship class.
         
     
     def run_game(self):
@@ -19,6 +21,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:                           
                     sys.exit()                                          #quit the game
             self.screen.fill(self.settings.bg_color)                    # Redraw the screen during each pass through the loop.
+            self.ship.blitme()                                          # Draw the ship on the screen.
             pygame.display.flip()                                       # Make the most recently drawn screen visible.
             self.clock.tick(60)                                         # Set the frame rate to 60 frames per second.
 
--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ class Settings:
     def __init__(self):
         self.screen_width = 600
         self.screen_height = 400
-        self.bg_color = (120, 81, 169)
+        self.bg_color = (233, 233, 233)     #(120, 81, 169) is a shade of purple.
         self.caption = "Alien Invasion 🎮"
